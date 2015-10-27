8b17b919c6609154f4d183823eeb70ea133c6c26
raosungang <raosungang@hotmail.com>
2015-10-27 21:33:10 +0800

    reg change

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', {title: '首页'});
+});
+
+
+router.get('/login', function (req, res) {
+    res.render('login', {title: '登陆'});
+});
+
+router.post('/login', function (req, res) {
+
+});
+
+router.get('/reg', function (req, res) {
+    res.render('reg', {title: '注册'})
+});
+
+router.post('/reg', function (req, res) {
+    var postData = {
+        name: req.body.name,
+        password: req.body.password
+    };
+    res.send('注册成功');
+    console.log(postData);
+});
+
+router.get('/logout', function (req, res) {
+    res.send('退出')
 });
 
 module.exports = router;
